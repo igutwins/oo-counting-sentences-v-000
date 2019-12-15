d80f1c2f4b15227b0d6c221ee8f0539994be40bc
Ian Gutwinski <iangutwinski@gmail.com>
2019-12-15 15:43:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,13 +2,20 @@ require 'pry'
 
 class String
 
-def sentence?
-  self.end_with?(".")
-end
-def question?
-  self.end_with?("?")
-end
-def exclamation?
-  self.end_with?("!")
-end
+  def sentence?
+    self.end_with?(".")
+  end
+
+  def question?
+    self.end_with?("?")
+  end
+
+  def exclamation?
+    self.end_with?("!")
+  end
+
+  def count_sentences
+    self.split(/[.!?]/).length
+  end
+
 end
